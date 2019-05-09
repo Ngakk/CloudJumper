28c0a4dcca2d1b5fec13382ca3b302b0bc4f57b0
Ngakk <32682022+Ngakk@users.noreply.github.com>
2019-05-09 13:05:00 -0500

    Initial commit

---
+++ Assets/_main/Character/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float Velocity = 10;
+
+    private Rigidbody rigi;
+
+    private void Start()
+    {
+        rigi = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        float screenPointX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+
+        float dir = screenPointX - transform.position.x;
+
+        //transform.position += Vector3.right * dir * Time.deltaTime * Velocity;
+
+        rigi.velocity = new Vector3(dir * Velocity, rigi.velocity.y, rigi.velocity.z);
+    }
+}
---
+++ Assets/_main/Network/login.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class login : MonoBehaviour
+{
+    public string usuario;
+    public string password;
+
+    private void Start()
+    {
+        StartCoroutine(IELogin());
+    }
+
+    IEnumerator IELogin()
+    {
+        //WWWForm permite mandar datos a paginas web
+        WWWForm sendData = new WWWForm();
+        //Como se espera en POST del php ,, el dato o informacion a mandar
+        sendData.AddField("username", usuario);
+        sendData.AddField("assword", password);
+
+        //Mandamos informacion
+
+        //using (UnityWebRequest www = UnityWebRequest.Post("http://virtualmeatball.rf.gd/games/cloud-jumper/php/login.php", sendData))
+        using (UnityWebRequest www = UnityWebRequest.Get("http://virtualmeatball.rf.gd//test.php"))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+}
